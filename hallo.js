883b79fc6ec35d8e35d0ab5690c0f23199b8e3f6
mdtashin74 <125220575+mdtashin74@users.noreply.github.com>
2023-12-21 03:12:36 +0600

    sf

---
+++ hallo.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    rewind:true,
+    margin:390,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
